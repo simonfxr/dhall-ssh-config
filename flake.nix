354951e332878c074c0759fdfab4398afb2b15c1
Simon Reiser <me@sfxr.de>
2022-06-14 18:42:29 +0200

    nixify

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "dhall-ssh-config nix flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    let
+      mkApp = pkgs: extraArgs:
+        let
+          args = {
+            root = ./.;
+            name = "dhall-ssh-config";
+            returnShellEnv = false;
+            # cabal2nixOptions =
+            #   "--flag=svg --flag=cairo --flag=-rasterific --flag=-postscript --flag=-ps";
+          } // extraArgs;
+          pkg = pkgs.haskellPackages.developPackage args;
+          withCabal = pkg:
+            pkg.overrideAttrs (attrs: {
+              buildInputs = attrs.buildInputs ++ [ pkgs.cabal-install ];
+            });
+        in if args.returnShellEnv then withCabal pkg else pkg;
+    in flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages.dhall-ssh-config = mkApp pkgs { };
+        packages.default = packages.dhall-ssh-config;
+        devShells.default = mkApp pkgs { returnShellEnv = true; };
+      }) // {
+        overlays.default = final: prev: { dhall-ssh-config = mkApp final { }; };
+      };
+}
