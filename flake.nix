4333fdb8d8a2cf8fcedb85fe3735412075bad134
Simon Reiser <me@sfxr.de>
2023-06-12 07:45:32 +0200

    bump

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "dhall-ssh-config nix flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
